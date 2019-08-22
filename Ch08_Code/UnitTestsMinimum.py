49d32eb55c02a3d328d61493a42e596f5528c2cc
BurkhardAMeier <BurkhardAMeier@gmail.com>
2019-08-22 15:00:17 -0400

    Add files via upload

---
+++ Ch08_Code/UnitTestsMinimum.py
@@ -0,0 +1,14 @@
+'''
+Created on Jun 9, 2019
+Ch08
+@author: Burkhard A. Meier
+'''
+
+import unittest 
+ 
+class GuiUnitTests(unittest.TestCase): 
+    pass 
+ 
+if __name__ == '__main__': 
+    unittest.main() 
+    
---
+++ Ch08_Code/UnitTests_One.py
@@ -0,0 +1,22 @@
+'''
+Created on Jun 9, 2019
+Ch08
+@author: Burkhard
+'''
+
+import unittest
+from Ch08_Code.LanguageResources import I18N
+
+class GuiUnitTests(unittest.TestCase):
+    
+    def test_TitleIsEnglish(self):
+        i18n = I18N('en')
+        self.assertEqual(i18n.title, 
+                         "Python Graphical User Interface")
+        
+#==========================                          
+if __name__ == '__main__':
+    unittest.main()
+    
+    
+
