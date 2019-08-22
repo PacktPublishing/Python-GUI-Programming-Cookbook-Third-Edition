003c08249b2ddc7039a1089166aa6ea1a19e6260
BurkhardAMeier <BurkhardAMeier@gmail.com>
2019-08-22 14:57:51 -0400

    Add files via upload

---
+++ Ch05_Code/Matplotlib_our_first_chart.py
@@ -0,0 +1,15 @@
+'''
+Created on Jul 26, 2019
+Chapter 05
+@author: Burkhard
+'''
+
+
+import matplotlib.pyplot as plt
+from pylab import show
+
+x_values = [1,2,3,4] 
+y_values = [5,7,6,8] 
+plt.plot(x_values, y_values)
+
+show()              
